1d6a955f20391b65c033113b331ad9dc944c91ca
Michael Martin <ss013r@gmail.com>
2022-12-09 17:49:59 -0500

    Fixed pipe logic (recv() is blocking)

--- flaskapp.py
+++ flaskapp.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request, g
 import multiprocessing
 
-conn1, conn2 = multiprocessing.Pipe(duplex=True)
-stateConn1, stateConn2 = multiprocessing.Pipe(duplex=True) #current system state
-
-secretCode = "1234"
+#system/webserver pipe for sending secret code between processes
+conn1, conn2 = multiprocessing.Pipe(duplex=False) #system is receive-only
+#system/webserver pipe for sending current state between processes
+stateConn2, stateConn1 = multiprocessing.Pipe(duplex=False) #webserver is receive-only
+#secret code that disarms system
+SECRET_CODE = "1234"
+#initial state of system at boot; this gets changed by system().
+INITIAL_STATE = "disarmed"
 
 app = Flask(__name__)
 
@@ -18,52 +22,47 @@ def index():
 
 @app.route('/', methods=['POST'])
 def display_input():
-    user_input = request.form['user_input']
+    user_input = request.form['user_input'] #the user's input for the secret code
     conn2.send(user_input)
     return render_template('ActiveHTML.html', user_input=user_input, current_state=stateConn2.recv())
 
 def system():
-    
-    current_state = "disarmed"
-    stateConn1.send(current_state) #send the state immediately
-
+    current_state = INITIAL_STATE
+    stateConn1.send(current_state) #send the default state for web server right away
     while(1):
-        #handle case: system is disarmed, set it to armed if code is input on webpage
+        #Handle case: System is disarmed, set it to armed if code is input on webpage
         if current_state == "disarmed":
             print("disarmed")
             while(1):
-                if conn1.recv() == secretCode:
+                if conn1.recv() == SECRET_CODE:
                     print("Code accepted while disarmed, arming...")
                     current_state = "armed"
-                    stateConn1.send(current_state)
                     break
                 else:
                     print("Incorrect code sent while disarmed.")
-                    stateConn1.send(current_state)
                     break
 
-        #handle case: system is armed, set it to disarmed if code is input on webpage
+        #Handle case: System is armed, set it to disarmed if code is input on webpage
         elif current_state == "armed":
             print("armed")
             while(1):
-                if conn1.recv() == secretCode:
+                if conn1.recv() == SECRET_CODE:
                     print("Code accepted while armed, disarming...")
                     current_state = "disarmed"
-                    stateConn1.send(current_state)
                     break
                 else:
                     print("Incorrect code sent while armed.")
-                    stateConn1.send(current_state)
                     break
 
-
-        #handle case: system is triggered, set it to disarmed if code is input on keypad
+        #Handle case: System is triggered, set it to disarmed if code is input on keypad
         elif current_state == "triggered":
             print("triggered")
 
-        #handle case: system is alerted, send out the photo and maybe do other things?
+        #Handle case: System is alerted, send out the photo and maybe do other things?
         elif current_state == "alert":
             print("alert")
+
+        stateConn1.send(current_state) #end of system loop: send current state
             
 
 def run_website():
