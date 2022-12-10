647f6782eb00df13dad991b4694746030fa062f3
Michael Martin <ss013r@gmail.com>
2022-12-09 22:17:59 -0500

    Overhaul of system() loop. Should have no hangs.

--- flaskapp.py
+++ flaskapp.py
@@ -1,20 +1,26 @@
 from flask import Flask, render_template, request, g
 import multiprocessing
+import time
 
-#system/webserver pipe for sending secret code between processes
+#system/webserver pipe for sending secret code between processes:
 conn1, conn2 = multiprocessing.Pipe(duplex=False) #system is receive-only
-#system/webserver pipe for sending current state between processes
+#system/webserver pipe for sending current state between processes:
 stateConn2, stateConn1 = multiprocessing.Pipe(duplex=False) #webserver is receive-only
-#secret code that disarms system
+#secret code that disarms system:
 SECRET_CODE = "1234"
-#initial state of system at boot; this gets changed by system().
+#initial state of system at boot; this gets changed by system():
 INITIAL_STATE = "disarmed"
+#Maximum time webserver will have to wait for response, in seconds:
+POLLING_INTERVAL = 0.1
+#Default input code to be sent when no POST is made:
+BLANK_CODE = ""
 
 app = Flask(__name__)
 
 @app.route('/') #root directory for website
 def index():
-    return render_template('ActiveHTML.html', user_input="",current_state=stateConn2.recv())
+    conn2.send(BLANK_CODE)
+    return render_template('ActiveHTML.html', user_input=BLANK_CODE, current_state=stateConn2.recv())
 
 @app.route('/', methods=['POST']) #root directory for website, POST
 def display_input():
@@ -24,41 +30,50 @@ def display_input():
 
 def system():
     current_state = INITIAL_STATE
-    stateConn1.send(current_state) #send the default state for web server right away
     while(1):
-        #Handle case: System is disarmed, set it to armed if code is input on webpage
-        if current_state == "disarmed":
-            print("disarmed")
-            while(1):
-                if conn1.recv() == SECRET_CODE:
-                    print("Code accepted while disarmed, arming...")
-                    current_state = "armed"
-                    break
-                else:
-                    print("Incorrect code sent while disarmed.")
-                    break
+        #Case 1: System is disarmed, set it to armed if secret code is input on webpage
+        while(current_state == "disarmed"):
+            print("current_state set to disarmed")
+            #Block System process until a webserver request:
+            receivedCode = conn1.recv() 
+            if receivedCode == SECRET_CODE: #correct code received
+                print("Code accepted while disarmed, arming...")
+                current_state = "armed"
+            elif receivedCode == BLANK_CODE: #no code received
+                print("Blank code, no POST.")
+            else:
+                print("Incorrect code sent while disarmed.") #bad code received
+            stateConn1.send(current_state)
 
-        #Handle case: System is armed, set it to disarmed if code is input on webpage
-        elif current_state == "armed":
-            print("armed")
-            while(1):
-                if conn1.recv() == SECRET_CODE:
+        #Case 2: System is armed, set it to disarmed if code is input on webpage,
+        #...but will be able to be triggered by sensors as well.
+        while(current_state == "armed"):
+            print("current_state set to armed")
+            receivedCode = conn1.poll() #Avoid blocking, poll for webserver request first
+            if receivedCode is not None:
+                #Webserver request came in, handle it:
+                receivedCode = conn1.recv() #flush pipe
+                if receivedCode == SECRET_CODE:
                     print("Code accepted while armed, disarming...")
                     current_state = "disarmed"
-                    break
+                elif receivedCode == BLANK_CODE:
+                    print("Blank code, no POST.")
                 else:
                     print("Incorrect code sent while armed.")
-                    break
+                stateConn1.send(current_state)
+            else:
+                #Do other things related to sensor/keypad polling here
+                time.sleep(POLLING_INTERVAL)
+                continue
 
-        #Handle case: System is triggered, set it to disarmed if code is input on keypad
-        elif current_state == "triggered":
-            print("triggered")
+        #Case 3: System is triggered, set it to disarmed if code is input on keypad
+        while(current_state == "triggered"):
+                print("current_state set to triggered")
 
-        #Handle case: System is alerted, send out the photo and maybe do other things?
-        elif current_state == "alert":
-            print("alert")
+        #Case 4: System is alerted, send out the photo and maybe do other things?
+        while(current_state == "alert"):
+                print("current_state set to alert")
 
-        stateConn1.send(current_state) #end of system loop: send current state
             
 
 def run_website():
